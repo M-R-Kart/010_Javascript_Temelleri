c0cbae3f93478a15b73e413d728fdea44964baf5
MRKart <rafiikart@icloud.com>
2024-10-01 11:13:19 +0300

    Update 3_oparetorler.js

--- 3_oparetorler.js
+++ 3_oparetorler.js
@@ -36,13 +36,13 @@ veri =a!=b; // true //! (a, b ye eşit değil)
 veri = a!==b; // true //! (a, b ye eşittir ve türleri aynıdır.)
 veri = a>b; // true //! (a, b den büyük)
 veri = b<c; // false //! (b, c den büyük)
-veir = 5 >= 5; // true //! (5, 5 eşit)
+veri = 5 >= 5; // true //! (5, 5 eşit)
 veri = b <= c; // false //! (b, c den küçük eşit değil)
 
 //* 4. Mantıksal Oparatörler (and, or, not) (&&, ||,!)
 
 veri = (a>b) && (a>c); // true //! (a, b den büyük ve a, c den büyük)
-veir = (b>a) || (a>c); // true //! (b, a den büyük değil ama (veya) a, c den büyük)
+veri = (b>a) || (a>c); // true //! (b, a den büyük değil ama (veya) a, c den büyük)
 veri = !(a>b); // false //! (a, b den büyük ama (! not) tersini almış olur)
 veri = !(b>a); // true //! (b, a den büyük değil (! not) tersini almış olur)
 
